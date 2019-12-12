3166efe67e2b70e910d3931b23682b3df9f00f5e
Oleg Galbert <oleg.galbert@cyberark.com>
2019-12-12 15:31:03 +0200

    .

--- game15/src/main.rs
+++ game15/src/main.rs
@@ -1,8 +1,20 @@
 use std::io;
 
+fn print_line(row: &[i32]) {
+    println!(".----.----.----.");
+    println!("| {:02} | {:02} | {:02} |", row[0], row[1], row[2]);
+    println!(".----.----.----.");
+}
+
 fn main() {
     println!("Starting 15 game");
 
+    println!();
 
+    let row: [i32; 3] = [1,2,3];
+
+    print_line(&row);
+
+    println!();
     println!("End game");
 }
