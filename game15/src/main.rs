751b7bfe1bd5e0dcf854031f689a0222a299f61b
Oleg Galbert <ogcraft@gmail.com>
2019-12-14 12:56:24 +0200

    .

--- game15/src/main.rs
+++ game15/src/main.rs
@@ -1,9 +1,7 @@
 use std::io;
 
 fn print_line(row: &[i32]) {
-    println!(".----.----.----.");
-    println!("| {:02} | {:02} | {:02} |", row[0], row[1], row[2]);
-    println!(".----.----.----.");
+    println!("| {:02} | {:02} | {:02} | {:02} |", row[0], row[1], row[2], row[3]);
 }
 
 fn main() {
@@ -11,9 +9,20 @@ fn main() {
 
     println!();
 
-    let row: [i32; 3] = [1,2,3];
+    let row1: [i32; 4] = [1,2,3,4];
+    let row2: [i32; 4] = [5,6,7,8];
+    let row3: [i32; 4] = [9,10,11,12];
+    let row4: [i32; 4] = [13,14,15,0];
 
-    print_line(&row);
+    println!("o----o----o----o----o");
+    print_line(&row1);
+    println!("o----o----o----o----o");
+    print_line(&row2);
+    println!("o----o----o----o----o");
+    print_line(&row3);
+    println!("o----o----o----o----o");
+    print_line(&row4);
+    println!("o----o----o----o----o");
 
     println!();
     println!("End game");
